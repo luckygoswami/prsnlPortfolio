6ace9434bf28411ad77735920a041fcfba8d53b6
Lucky Goswami <lgoswami204@gmail.com>
2023-06-15 14:20:57 +0530

    fix: fixed the typing animation stucking

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var typed = new Typed('#name', {
-    strings: ['Lucky Goswami'],
-    typeSpeed: 50,
-});
+
+setTimeout(function() {
+    // your animation code here
+    var typed = new Typed('#name', {
+        strings: ['Lucky Goswami'],
+        typeSpeed: 50,
+    });
+  }, 1000);
+  
