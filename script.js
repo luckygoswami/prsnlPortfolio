3fbdb35da0edc8dc2484785e221a30731938cb9f
Lucky Goswami <lgoswami204@gmail.com>
2023-06-16 10:46:24 +0530

    update: page 1 added

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-
-setTimeout(function() {
+setTimeout(function () {
     // your animation code here
     var typed = new Typed('#name', {
         strings: ['Lucky Goswami'],
         typeSpeed: 50,
     });
-  }, 1000);
-  
+}, 1000);
+
+setTimeout(function () {
+    // your animation code here
+    gsap.to("#circle", {
+        rotate: 0,
+        ease: Expo.easeInOut,
+        duration: 2
+    })
+    
+    var active = 3;
+    
+    var mncircles = document.querySelectorAll("#mncircle")
+    var sec = document.querySelectorAll(".sec")
+    
+    gsap.to(mncircles[active - 1], {
+        opacity: .5
+    })
+    
+    gsap.to(sec[active - 1], {
+        opacity: 1
+    })
+    
+    mncircles.forEach(function (val, index) {
+        val.addEventListener("click", function () {
+            gsap.to("#circle", {
+                rotate: (3 - (index + 1)) * 10,
+                ease: Expo.easeInOut,
+                duration: 1
+            })
+            greyout();
+            gsap.to(this, {
+                opacity: .5
+            })
+    
+            gsap.to(sec[index], {
+                opacity: 1
+            })
+        })
+    })
+    
+    function greyout() {
+        gsap.to(mncircles, {
+            opacity: .08
+        })
+        gsap.to(sec, {
+            opacity: .4
+        })
+    }
+}, 5000);
+
